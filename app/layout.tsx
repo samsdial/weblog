355b4b92458862550576ae0b2ab3520c90071087
Sergio Andres Martinez E <sergio.martinez@fracttal.com>
2024-08-12 08:23:32 -0500

    add¡

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
+// lay
+import { Providers } from "@/components/providers";
+import { SiteHeader } from "@/components/site-header";
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +20,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn("min-h-screen bg-background font-sans antialiased", inter.variable)}>
+        <Providers>
+          <div className="relative flex min-h-dvh flex-col bg-background">
+            <SiteHeader />
+            <main className="flex-1">
+              {children}
+            </main>
+          </div>
+        </Providers>
+      </body>
     </html>
   );
 }
---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+    name: "Sm Blog",
+    url: "http://example.com",
+    description: "nextjs blog",
+    author: "sams",
+    links: {
+        twitter: "http://twitter.com/",
+        github: "http://github.com/",
+        personalSite: "http://personal.com",
+    },
+}
+
+export type SiteConfig = typeof siteConfig
